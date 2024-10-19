a60f658b918dee142fe19f31505313ea1856b8a7
Alexis Mariqueo <alexismariqueo48@gmail.com>
2024-10-19 18:38:56 -0300

    nuevas

--- TRABAJO_PRACTICO_N5/Ejercicio_9/MonedaEEUU.py
+++ TRABAJO_PRACTICO_N5/Ejercicio_9/MonedaEEUU.py
@@ -3,7 +3,7 @@ from DecoratorMonedaEEUU import DecoratorMonedaEEUU
 class MonedaEEUU(DecoratorMonedaEEUU):
 
     def get_line_description(self):
-        return self._producto_decorado.get_line_description() + "$USD"
+        return self._producto_decorado.get_line_description() + "USD"
     
     def get_precio(self):
         return self._producto_decorado.get_precio()
